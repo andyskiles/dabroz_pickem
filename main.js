449384b0d65f3b4c13196aebcedfd80a64eaa529
Andrew Skiles <networking@andyskiles.com>
2015-08-09 23:57:47 -0400

    Add routes

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-Tasks = new Mongo.Collection('tasks');
 Matches = new Mongo.Collection('matches');
 Picks = new Mongo.Collection('picks');
 
@@ -9,121 +8,182 @@ if (Meteor.isClient) {
   });
 
   // only runs on client
-  angular.module('pickem', ['angular-meteor']);
+  angular.module('pickem', [
+    'angular-meteor', 
+    'ui.router', 
+    'ngAnimate'
+  ]);
 
-  angular.module('pickem').controller('PickemCtrl', ['$scope', '$meteor',
-    function ($scope, $meteor) {
+  angular.module('pickem').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+    function($urlRouterProvider, $stateProvider, $locationProvider){
 
-      $scope.picks = $meteor.collection(function() {
-        return Picks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
-      });
+      $locationProvider.html5Mode(true);
 
-      $scope.$watch('doNothing', function() {
-        if ($scope.doNothing)
-          $scope.query = {there: {$ne: true}};
-        else
-          $scope.query = {};
-      });
+      $stateProvider
+        .state('matchday1', {
+          url:'/matchday1',
+          templateUrl: 'matchday-1.ng.html',
+          controller: 'Matchday1Ctrl'
+        })
 
-      $scope.addPick0 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 0,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+        .state('matchday2', {
+          url:'/matchday2',
+          templateUrl: 'matchday-2.ng.html',
+          controller: 'Matchday2Ctrl'
+        })
 
-      $scope.addPick1 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 1,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+        .state('matchday3', {
+          url:'/matchday3',
+          templateUrl: 'matchday-3.ng.html',
+          controller: 'Matchday3Ctrl'
+        })
 
-      $scope.addPick2 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 2,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+        $urlRouterProvider.otherwise('/matchday1');
+    } 
 
-      $scope.addPick3 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 3,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+  ])
 
-      $scope.addPick4 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 4,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+  angular.module('pickem').controller('Matchday1Ctrl', ['$scope', '$stateParams', 
+    function($scope, $stateParams){
+      $scope.matchdayId = $stateParams.matchdayId;
+    }
+  ])
 
-      $scope.addPick5 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 5,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+  angular.module('pickem')
+    .controller('PickemCtrl', ['$scope', '$meteor',
+      function ($scope, $meteor) {
 
-      $scope.addPick6 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 6,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+        $scope.picks = $meteor.collection(function() {
+          return Picks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
+        });
 
-      $scope.addPick7 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 7,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+        $scope.$watch('doNothing', function() {
+          if ($scope.doNothing)
+            $scope.query = {there: {$ne: true}};
+          else
+            $scope.query = {};
+        });
 
-      $scope.addPick8 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 8,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+        $scope.addPick0 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 0,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
 
-      $scope.addPick9 = function(newPick) {
-        $scope.picks.push( {
-          text: newPick,
-          createdAt: new Date(),
-          id: 9,
-          owner: Meteor.userId(),
-          username: Meteor.user().username }
-        );
-      };
+        $scope.addPick1 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 1,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
 
+        $scope.addPick2 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 2,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
+
+        $scope.addPick3 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 3,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
+
+        $scope.addPick4 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 4,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
+
+        $scope.addPick5 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 5,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
+
+        $scope.addPick6 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 6,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
+
+        $scope.addPick7 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 7,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
+
+        $scope.addPick8 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 8,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
+
+        $scope.addPick9 = function(newPick) {
+          $scope.picks.push( {
+            text: newPick,
+            createdAt: new Date(),
+            id: 9,
+            owner: Meteor.userId(),
+            username: Meteor.user().username }
+          );
+        };
+
+      }])
+
+    .animation('.slide', [function() {
+      return {
+        // make note that other events (like addClass/removeClass)
+        // have different function input parameters
+        enter: function(element, doneFn) {
+          jQuery(element).fadeIn(1000, doneFn);
+
+          // remember to call doneFn so that angular
+          // knows that the animation has concluded
+        },
+
+        move: function(element, doneFn) {
+          jQuery(element).fadeIn(1000, doneFn);
+        },
+
+        leave: function(element, doneFn) {
+          jQuery(element).fadeOut(1000, doneFn);
+        }
+      }
     }]);
 }
