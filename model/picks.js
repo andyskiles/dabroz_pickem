0b1c0ee6b7c33b4fd5dec411af260a880dbdff3f
Andrew Skiles <networking@andyskiles.com>
2015-08-16 15:04:51 -0400

    Modify server

--- model/picks.js
+++ model/picks.js
@@ -1,12 +1,2 @@
 Matches = new Mongo.Collection('matches');
 Picks = new Mongo.Collection('picks');
-
-if (Meteor.isServer) {
-	Meteor.publish('picks', function () {
-		return Picks.find();
-	});
-}
-
-if (Meteor.isClient) {
-	Meteor.subscribe('picks');
-}
